0b77c0a8d9de6c3492a2fcd2dc8f6a9da459b59b
ponghao <ponghao@outlook.com>
2016-12-03 01:30:35 +0800

    - 修改版

--- CarPlateDetect/CarPlateDetector.py
+++ CarPlateDetect/CarPlateDetector.py
@@ -10,18 +10,54 @@ import cv2
 import numpy as np
 import sys
 
-def filterRect(cnt):    
-    rect = cv2.minAreaRect(cnt)  
-    box = cv2.boxPoints(rect) 
-    box = np.int0(box)  
+def filterRect(box):    
+
     output = False
 
     w = rect[1][0]
     h = rect[1][1]
     
-    if w > 0 and h > 0 and w * h > 1000:
-        output = True
+    if w < h:
+        w = rect[1][1]
+        h = rect[1][0]
+       
+    if w > 0 and h > 0: 
+        area = w * h
+        if w / h > 1.2 and w / h < 5 and area > 1000 and area < 80000:
+            output = True
     
+    return output
+
+def generate_seeds(center, width, height):
+    minsize = int(min(width, height))
+    minsize = int(minsize - minsize * 0.7)
+    
+    seed = [None] * 5
+    for i in range(5):
+        random_integer1 = np.random.randint(1000)
+        random_integer2 = np.random.randint(1000)
+        seed[i] = (center[0] + random_integer1 % int(minsize / 2) - int(minsize / 2), center[1] + random_integer2 % int(minsize / 2) - int(minsize / 2))
+    return seed
+
+def generate_mask(image, seed_point):
+    h = image.shape[0]
+    w = image.shape[1]
+    # OpenCV wants its mask to be exactly two pixels greater than the source image.
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    # We choose a color difference of (50,50,50). Thats a guess from my side.
+    lodiff = 50
+    updiff = 50
+    connectivity = 4
+    newmaskval = 255
+    flags = connectivity + (newmaskval << 8) + cv2.FLOODFILL_FIXED_RANGE + cv2.FLOODFILL_MASK_ONLY
+    _ = cv2.floodFill(image, mask, seed_point, (255, 0, 0), (lodiff, lodiff, lodiff), (updiff, updiff, updiff), flags)
+    return mask
+
+def rmsdiff(im1, im2):
+    diff=im1-im2
+    output=False
+    if np.sum(abs(diff))/float(min(np.sum(im1), np.sum(im2)))<0.3:
+        output=True
     return output
 
 if __name__ == '__main__':
@@ -33,68 +69,94 @@ if __name__ == '__main__':
     inputImg = sys.argv[1]
     outputImg = sys.argv[2]
     
-    img = cv2.imread(inputImg)
-    
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    imgRGBA = cv2.imread(inputImg)
 
-    #remove noise
-    noise_removal = cv2.bilateralFilter(img_gray, 8, 75, 75)
-    
-    # Histogram equalisation for better results
-    equal_histogram = cv2.equalizeHist(noise_removal)
+    imgGray = cv2.cvtColor(imgRGBA, cv2.COLOR_RGB2GRAY)
 
+    imgBlur = cv2.blur(imgGray, (5, 5))
+
+    equal_histogram = cv2.equalizeHist(imgBlur)
+    
     sobelx = cv2.Sobel(equal_histogram, cv2.CV_8U, 1, 0, ksize=3)
     
-    # Thresholding the image
-    ret, thresh_image = cv2.threshold(sobelx, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    ret, thresh_image = cv2.threshold(sobelx, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 3))
+    mor = cv2.morphologyEx(thresh_image, cv2.MORPH_CLOSE, element)
         
-    element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 9))
-    element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (24, 6))
-    element3 = cv2.getStructuringElement(cv2.MORPH_RECT, (23, 2))
-    
-    dilation = cv2.dilate(thresh_image, element2, iterations=1)
-    erosion = cv2.erode(dilation, element1, iterations=1)
-    #cv2.imshow("dilation", erosion)
-    
-#     mor = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, element3)
-#     cv2.imshow("morphologyEx1", closing)
-    mor = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, element3)
-#     cv2.imshow("morphologyEx2", closing)
-    
-    canny_image = cv2.Canny(mor, 60, 180, apertureSize=3)
-    # cv2.imshow("Canny", canny_image)
-    
-    new, contours, _ = cv2.findContours(canny_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    new, contours, _ = cv2.findContours(mor, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
     contoursImg = None
-    failedBox = None
-    plateMask = np.zeros(img_gray.shape, np.uint8)
-    cpImg = img.copy()
+    plateMask = np.zeros(imgGray.shape, np.uint8)
 
+    validateRect = []
     for cnt in contours:
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        
-        if filterRect(cnt):    
-            contoursImg = cv2.drawContours(plateMask, [box], 0, 255, -1,)
-    
-    
-    contoursImg = cv2.bitwise_and(img, img, mask=plateMask)
+        if filterRect(box):
+            contoursImg = cv2.drawContours(plateMask, [box], 0, 255, -1)
+            validateRect.append(rect)
+    contoursImg = cv2.bitwise_and(imgRGBA, imgRGBA, mask=plateMask)
 
-    if contoursImg is not None:
-        invertMask = cv2.bitwise_not(plateMask)     
-        bg = np.full(img.shape, 255, np.uint8)
-        bg = cv2.bitwise_and(bg, bg, mask=invertMask)
-        finalImg = cv2.bitwise_or(contoursImg, bg)
-        
-        h, w = img.shape[:2]
-        a = np.full((h, w, 1), 255, np.uint8)
-        a = cv2.bitwise_and(a, a, mask=plateMask)
-        b, g, r = cv2.split(finalImg)
-        bgra = [b, g, r, a]
-        finalImg = cv2.merge(bgra, 4)
-        cv2.imwrite(outputImg, finalImg)
+    print("validateRect = ", len(validateRect))
+    
+    mask_list = []
+    imgRGBAMask = imgRGBA.copy()
+    for rect in validateRect:
+        center = (int(rect[0][0]), int(rect[0][1]))
+        w = rect[1][0]
+        h = rect[1][0]
+        seeds = generate_seeds(center, w, h)
+         
+        for seed in seeds:
+            cv2.circle(imgRGBA, seed, 1, (0, 0, 255), -1)
+            msk = generate_mask(imgRGBAMask, seed)
+ 
+            contour = np.argwhere(msk.transpose()== 255)
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            if filterRect(box):
+                mask_list.append(msk)
+             
+    print("mask_list = ", len(mask_list))
+
+    final_masklist=[]
+    index=[]
+    for i in range(len(mask_list)-1):
+        for j in range(i+1, len(mask_list)):
+            if rmsdiff(mask_list[i], mask_list[j]):
+                index.append(j)
+                 
+    for mask_no in list(set(range(len(mask_list)))-set(index)):
+        final_masklist.append(mask_list[mask_no])
+         
+    print("final_masklist = ", len(final_masklist))
+    
+    mskIdx = 0
+    idx = outputImg.find(".png")
+    subName = outputImg[:idx]
+    for msk in final_masklist:
+        outputImg = subName +  "%d.png" % mskIdx
+        cv2.imwrite(outputImg, msk)
+        mskIdx += 1
+#     plateMask = np.zeros(imgGray.shape, np.uint8)
+#     for msk in final_masklist:
+#         contour=np.argwhere(msk.transpose()==255)
+#         rect=cv2.minAreaRect(contour)
+#         box = cv2.boxPoints(rect)
+#         box = np.int0(box)
+#         plateMask = final_masklist[i]
+
+    h, w = imgRGBA.shape[:2]
+    imgA8 = np.full((h, w, 1), 255, np.uint8)
+    imgA8 = cv2.bitwise_and(imgA8, imgA8, mask=plateMask)
+    b, g, r = cv2.split(contoursImg)
+    bgra = [b, g, r, imgA8]
+    finalImg = cv2.merge(bgra, 4)
+            
+    cv2.imwrite(outputImg, finalImg)
     
     cv2.waitKey()
     
