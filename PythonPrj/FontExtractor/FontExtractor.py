bba6aa85d147c33c3df692612f7096960c757c7f
ponghao <ponghao@outlook.com>
2016-12-29 02:22:44 +0800

    - 改变颜色

--- PythonPrj/FontExtractor/FontExtractor.py
+++ PythonPrj/FontExtractor/FontExtractor.py
@@ -8,6 +8,7 @@ Created on 2016.12.28
 http://jingyan.baidu.com/article/20b68a88be3263796cec62d0.html
 http://stackoverflow.com/questions/20670761/letter-inside-letter-pattern-recognition
 https://www.learnopencv.com/filling-holes-in-an-image-using-opencv-python-c/
+http://stackoverflow.com/questions/29125349/change-color-of-a-pixel-with-opencv
 '''
 
 import cv2
@@ -53,16 +54,20 @@ if __name__ == '__main__':
     
     fontMask = cv2.bitwise_and(binary, binary, mask=mask)
     fontMask = np.int8(fontMask)
-    
+      
     imgRGBA = cv2.bitwise_and(imgRGBA, imgRGBA, mask=fontMask)
-    
     imgA8 = np.full((mskH, mskW, 1), 255, np.uint8)
     imgA8 = cv2.bitwise_and(imgA8, imgA8, mask=fontMask)
-    
+              
     b, g, r = cv2.split(imgRGBA)
     bgra = [b, g, r, imgA8]
     finalImg = cv2.merge(bgra, 4)
-    cv2.imshow("imgRGBA.png", finalImg)    
+
+    img_gray = cv2.cvtColor(finalImg,cv2.COLOR_BGR2GRAY)
+    new=[[[255%(j + 1), 255%(j + 1), j] for j in i] for i in img_gray]
+    dt = np.dtype('f8')
+    new=np.array(new,dtype=dt)
+    cv2.imwrite("imgRGBA.png", new)    
     
     cv2.waitKey()
 
