26320913c9c14ef940eb7adf3591c7577a53f14d
ponghao <ponghao@outlook.com>
2016-12-29 02:04:37 +0800

    - 提取毛笔字

--- PythonPrj/FontExtractor/FontExtractor.py
+++ PythonPrj/FontExtractor/FontExtractor.py
@@ -15,60 +15,18 @@ import cv2
 import numpy as np
 
 
-def flood_fill_color(img, rects):
-
-    mskH = img.shape[0] + 2
-    mskW = img.shape[1] + 2
-    
-    final_mask = np.zeros((mskH, mskW), np.uint8)
-     
-    for rect in rects:
-        center = (int(rect[0][0]), int(rect[0][1]))
-        w = rect[1][0]
-        h = rect[1][0]
-   
-        minsize = int(min(w, h))
-        minsize = int(minsize * 0.2)
-        mask = np.zeros((mskH, mskW), np.uint8)
-        
-        lodiff = 50
-        updiff = 50
-        connectivity = 8
-        newMaskVal = 255
-        numSeeds = 10
-        flags = connectivity | cv2.FLOODFILL_MASK_ONLY | cv2.FLOODFILL_FIXED_RANGE | (newMaskVal << 8)
-        for i in range(numSeeds):
-            x = center[0] + np.random.randint(1000) % minsize - int(minsize / 2)
-            y = center[1] + np.random.randint(1000) % minsize - int(minsize / 2)
-            seed = (x, y)
-                
-            try:
-                cv2.floodFill(img, mask, seed, (255, 0, 0), (lodiff, lodiff, lodiff), (updiff, updiff, updiff), flags)
-
-            except:
-                pass
-        
-            contours = np.argwhere(mask.transpose() == 255)
-            rect = cv2.minAreaRect(contours)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
- 
-            cv2.drawContours(final_mask, [box], 0, 255, -1)
-  
-    return final_mask
-
 if __name__ == '__main__':
     
-    ImgSRC = cv2.imread("2.jpg")
+    ImgSRC = cv2.imread("font.jpg")
     imgRGBA = ImgSRC.copy()
     
     imgGray = cv2.cvtColor(imgRGBA, cv2.COLOR_RGB2GRAY)
     imgBlur = cv2.blur(imgGray, (5, 5))
 
-    _, imgThresh = cv2.threshold(imgBlur, 128, 255, cv2.THRESH_BINARY_INV)
+    _, binary = cv2.threshold(imgBlur, 128, 255, cv2.THRESH_BINARY_INV)
     
     element = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 3))
-    mor = cv2.morphologyEx(imgThresh, cv2.MORPH_CLOSE, element)
+    mor = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, element)
     
     _, contours, _ = cv2.findContours(mor, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
@@ -82,47 +40,29 @@ if __name__ == '__main__':
         maxArea = area
         maxContour = cnt
     
+    mskH = imgRGBA.shape[0]
+    mskW = imgRGBA.shape[1]
+    mask = np.zeros((mskH, mskW), np.uint8)
+        
     if maxContour is not None:
         rect = cv2.minAreaRect(maxContour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         
-        cv2.drawContours(imgRGBA, [box], -1, (255, 0, 0), 2)
-        
-        w = rect[1][0]
-        h = rect[1][0]
-        center = (int(rect[0][0]), int(rect[0][1]))
-
-        lodiff = 80
-        updiff = 80
-        connectivity = 8
-        newMaskVal = 255
-        numSeeds = 10
-        flags = connectivity | cv2.FLOODFILL_MASK_ONLY | cv2.FLOODFILL_FIXED_RANGE | (newMaskVal << 8)
-
-        mskH = imgRGBA.shape[0] + 2
-        mskW = imgRGBA.shape[1] + 2
-        mask = np.zeros((mskH, mskW), np.uint8)
-        
-        minsize = int(min(w, h))
-        minsize = int(minsize * 0.3)
-        if minsize == 0:
-            minsize = 1
-            
-        for i in range(numSeeds):
-            x = center[0] + np.random.randint(1000) % minsize - int(minsize / 2)
-            y = center[1] + np.random.randint(1000) % minsize - int(minsize / 2)
-            seed = (x, y)
-            try:
-                cv2.floodFill(imgRGBA, mask, seed, 255, (lodiff, lodiff, lodiff), (updiff, updiff, updiff), flags)
-
-            except:
-                pass
-        
-#         cv2.floodFill(imgRGBA, mask, center, 255, (lodiff, lodiff, lodiff), (updiff, updiff, updiff), flags);
+        cv2.drawContours(mask, [box], 0, 255, -1)
     
-    cv2.imshow("imgRGBA", imgRGBA)        
-    cv2.imshow("mask", mask)
+    fontMask = cv2.bitwise_and(binary, binary, mask=mask)
+    fontMask = np.int8(fontMask)
+    
+    imgRGBA = cv2.bitwise_and(imgRGBA, imgRGBA, mask=fontMask)
+    
+    imgA8 = np.full((mskH, mskW, 1), 255, np.uint8)
+    imgA8 = cv2.bitwise_and(imgA8, imgA8, mask=fontMask)
+    
+    b, g, r = cv2.split(imgRGBA)
+    bgra = [b, g, r, imgA8]
+    finalImg = cv2.merge(bgra, 4)
+    cv2.imshow("imgRGBA.png", finalImg)    
+    
     cv2.waitKey()
-    cv2.destroyAllWindows()
-        
+
